311719bfd76a4183038a8a3f4ec52588c5057556
Anita Kapse <anita.kapse311@gmail.com>
2025-05-07 17:23:17 +0530

    My first commit

---
+++ src/session1/BookLibrary.java
@@ -0,0 +1,51 @@
+package session1;
+
+public class BookLibrary 
+{
+	public static void main(String args[])
+	{
+		/*int a , b , c;            //declaration
+		a=100;                    //assignment
+		b=200;
+		c=300;  */
+		
+		int a = 100 , b = 200 , c = 300; //Declaration & assignment
+		 
+		
+		
+		System.out.println("Value of a is :"+a);
+		
+		System.out.println("Value of b is :"+b);
+		
+		System.out.println("Value of c is :"+c);
+		
+		//System.out.println(a+ " "+ b +" " +c);
+		
+		System.out.println(a+b+c);
+		 
+		byte By=125;
+		System.out.println("Value of Byte is :" +By);		
+		
+		short Sh = 26209;
+		System.out.println("value Of Short is :" + Sh);
+		
+		float Fl = 387.08765f;
+		System.out.println("Value of Float is :" +Fl);
+		
+		double Db = 23.788933;
+		System.out.println("Value Of Double is :" +Db);
+		
+		long L=388755444430l;
+		System.out.println("Value of Long is :" +L);
+		
+		char Ch = 'A';
+		System.out.println("Value of Char " +Ch);
+		
+		String St = "Anita";
+		System.out.println("Name=" +St);
+		
+		
+	}
+	
+
+}
---
+++ src/session1/NewBookLibrary.java
@@ -0,0 +1,26 @@
+package session1;
+
+public class NewBookLibrary 
+{
+	public static void main(String args[])
+	{
+		String Title = "Mrutunjay";
+		String AuthorName = "Shivaji Satam";
+		
+		/*int Year of Publication = 1956;
+		float Price of Book = 500;
+		boolean Availability of Book = true;
+		String Name of Member = "Anita";
+		int Member Id = 1;
+		Char Type of Membership = 'A';
+		int Borrowing Period = 3;
+		float fine per extra day;
+		float Total fine; */
+		
+		System.out.println("Enter Book Title"+Title);
+		System.out.println("Enter Author Name" +AuthorName);
+		//System.out.println("Publication Year"+Year of Publication);
+	}
+	
+
+}
