91ea02d2fb0a8da063859c676627dfbb39bb230d
Anita Kapse <anita.kapse311@gmail.com>
2025-05-09 17:14:26 +0530

    SportDayEventProgram

---
+++ src/controlStatements/SportsDayEvent.java
@@ -0,0 +1,24 @@
+package controlStatements;
+
+public class SportsDayEvent {
+
+	public static void main(String[] args) 
+	{
+	    int athlete1=20;
+	    int athlete2=30;
+	   
+	   if (athlete1>athlete2)
+	    {
+	    	System.out.println("Athlete1 is winner");
+	    }
+	    else if(athlete1==athlete2)
+	    {
+	    	System.out.println("It's a Tie ");
+	    }
+	    else
+	    {
+	    	System.out.println("Athlete2 is winner");
+	   }
+	}
+	
+	}
