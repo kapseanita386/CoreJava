eac2d311ed4fba5bdb9a3f0c17398e6e41815d28
Anita Kapse <anita.kapse311@gmail.com>
2025-05-12 13:11:15 +0530

    DecendingOrderProgram

---
+++ src/loopingStatements/DecendingOrderNumbers.java
@@ -0,0 +1,16 @@
+package loopingStatements;
+
+public class DecendingOrderNumbers {
+
+	public static void main(String[] args) 
+	{
+		int i=10;
+		
+		while(i>=1)   //(i>0) 
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+
+}
