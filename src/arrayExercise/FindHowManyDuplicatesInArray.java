61571360dcb52c6ad722945092f1dfcbac3b313c
Anita Kapse <anita.kapse311@gmail.com>
2025-05-19 14:01:27 +0530

    Find how many duplicates in array

---
+++ src/arrayExercise/FindHowManyDuplicatesInArray.java
@@ -0,0 +1,24 @@
+package arrayExercise;
+
+public class FindHowManyDuplicatesInArray {
+
+	public static void main(String[] args) 
+	{
+		
+		int a[]= {100,200,100,300,100,100};	
+		
+		int num=1200;
+		int count=0;
+		
+		for(int i=0;i<=a.length-1;i++)
+		{
+			if(a[i]==num)
+			{
+				
+				count++;
+			}
+		}
+		System.out.println(+num +" " +"number is repeated "+count+" "+"times");
+	}
+
+}
